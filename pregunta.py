80513a3fb3e4594090a7ea3c64fd837c4096088f
salperezate <salperezate@unal.edu.co>
2023-04-11 21:32:39 -0500

    Quiz 4

--- pregunta.py
+++ pregunta.py
@@ -13,9 +13,22 @@ import pandas as pd
 
 
 def ingest_data():
+    
+    df=pd.read_fwf('clusters_report.txt',widths=[9,16,16,77])
+    df=df.fillna(method='ffill')
+    df=df.groupby(df.columns[0]).agg({df.columns[1]: 'first', df.columns[2]: 'first', df.columns[3]: lambda x: ' '.join(x)}).reset_index()
+    df=df.rename(columns={'Cluster':'cluster','Cantidad de':'cantidad_de_palabras_clave','Porcentaje de':'porcentaje_de_palabras_clave','Principales palabras clave':'principales_palabras_clave'})
+    df.drop(0,axis=0,inplace=True)
+    
+    df['cluster'] = df['cluster'].astype(int)
+    df = df.sort_values('cluster')
+    df['cantidad_de_palabras_clave'] = df['cantidad_de_palabras_clave'].astype(int)
 
-    #
-    # Inserte su código aquí
-    #
+    df['porcentaje_de_palabras_clave']=df['porcentaje_de_palabras_clave'].str.replace('%', '', regex=True)
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].str.replace(',', '.', regex=True).astype(float)
+
+    df['principales_palabras_clave']=df['principales_palabras_clave'].str.replace('\s+', ' ', regex=True)
+    df['principales_palabras_clave']=df['principales_palabras_clave'].str.replace(',+', ',', regex=True)
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].apply(lambda x: x.replace('.', ''))
 
     return df
